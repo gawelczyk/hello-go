d9e57ad2cf618f0e794e89ebf3dae7c3ffffdfd8
Adam TG <adam@gawelczyk.com>
2022-03-06 22:31:20 +0100

    adding error handling adding logging

--- hello/hello.go
+++ hello/hello.go
@@ -2,16 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(log.Lmsgprefix | log.LstdFlags)
+
 	fmt.Println("Hello, World!")
 	fmt.Println("AQQ!!!")
 	fmt.Println(quote.Go())
 
-	message := greetings.Hello("Gladys")
+	var message string
+	var err error
+
+	message, err = greetings.Hello("Gladys")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
+
+	message, err = greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+
 }
